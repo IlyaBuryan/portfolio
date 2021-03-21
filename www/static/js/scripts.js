00085567a3b68424a922b0310978451cfb9bfd10
Ilya <forwow21@mail.ru>
2021-03-21 17:42:58 +0300

    defer

--- www/static/js/scripts.js
+++ www/static/js/scripts.js
@@ -1,26 +1,21 @@
 // Function for burger menu
-document.addEventListener("DOMContentLoaded", function () {
-    let burger = document.getElementsByClassName("header_menu_burger")[0];
-    let nav = document.getElementsByClassName("header_menu_navbar")[0];
-    burger.onclick = function () {
-        burger.classList.toggle("activated");
-        nav.classList.toggle("activated");
-    }
-});
+let burger = document.getElementsByClassName("header_menu_burger")[0];
+let nav = document.getElementsByClassName("header_menu_navbar")[0];
+burger.onclick = function () {
+    burger.classList.toggle("activated");
+    nav.classList.toggle("activated");
+}
 
 // Curriculum hide
-window.onload = (function () {
+$('.hide-btn').on('click', function (event) {
+    event.preventDefault();
+    $('.main_section_study_text_list_item').toggleClass('hidden');
+    $('.main_section_study_text_list_item_i').toggleClass('hidden');
+    $('.main_section_study_hr').toggleClass('hidden');
+    $('.main_section_study_hr').toggleClass('hidden');
+    $('.main_flex').toggleClass('main_flex_activ');
+    $('.main_done').toggleClass('main_done_activ');
 
-	$('.hide-btn').on('click', function (event) {
-		event.preventDefault();
-		$('.main_section_study_text_list_item').toggleClass('hidden');
-		$('.main_section_study_text_list_item_i').toggleClass('hidden');
-		$('.main_section_study_hr').toggleClass('hidden');
-		$('.main_section_study_hr').toggleClass('hidden');
-		$('.main_flex').toggleClass('main_flex_activ');
-		$('.main_done').toggleClass('main_done_activ');
-
-	})
-});
+})
 
 
